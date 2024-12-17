58f8e074c4cd331df5c21dbd468113a123377e14
Ali <alicode6.cs@gmail.com>
2024-12-17 23:46:30 +0500

    idk whats going on

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}`)
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
 
     console.log(`\nmongoDB connected! DB host: ${connectionInstance.connection.host}/${DB_NAME}`);
     
