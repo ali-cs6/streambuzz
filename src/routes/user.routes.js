54090ea048d3426c3fd49b7b94f90f8f0d4ccf82
Ali <alicode6.cs@gmail.com>
2025-02-18 18:46:21 +0500

    Routes added for all the user controllers, both unsecured and secured(with JWT)

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { registerUser, logOutUser } from "../controllers/user.controllers.js";
+import { registerUser, logOutUser, loginUser, refreshAccessToken, getCurrentUser, getUserChannelProfile, updateAccountDetails, updateUserAvatar, getWatchHistory } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import {verifyJWT} from "../middlewares/auth.middlewares.js";
 
 const router = Router();
+
+
+//unsecured routes
 
 // post req coz we are recieving data
 // multer for input files
@@ -21,7 +24,17 @@ router.route("/register").post(
   registerUser
 );
 
+router.route("/login").post(loginUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
 //secured routes
 router.route("/logout").post(verifyJWT, logOutUser)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/c/:usernme").get(verifyJWT, getUserChannelProfile)
+router.route("/updte-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserAvatar)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
